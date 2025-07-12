fbc87eabb6912c33c7b955aa2b76825080ee430d
Panose-pixel <juanangelalvarado327@gmail.com>
2025-07-12 14:59:25 -0500

    QR-12/07/2025

---
+++ static/js/script.js
@@ -0,0 +1,35 @@
+document.getElementById("btnMenu").addEventListener("click",
+    function () {
+      
+      let elemento = document.getElementById("navbar");
+      if (elemento.classList.contains("navbar")) {
+        elemento.classList.remove("navbar");
+        elemento.classList.add("no_navbar");
+      } else {
+        elemento.classList.remove("no_navbar");
+        elemento.classList.add("navbar");
+      }
+  
+    });
+    document.getElementById("btnMenu2").addEventListener("click",
+    function () {
+      
+      let elemento = document.getElementById("navbar2");
+      if (elemento.classList.contains("navbar2")) {
+        elemento.classList.remove("navbar2");
+        elemento.classList.add("no_navbar2");
+      } else {
+        elemento.classList.remove("no_navbar2");
+        elemento.classList.add("navbar2");
+      }
+  
+    });
+
+    function mostrarInstrucciones(elemento) {
+    document.querySelectorAll('.receta').forEach(el => {
+        if (el !== elemento) el.classList.remove('activa');
+    });
+
+    elemento.classList.toggle('activa');}
+
+    
